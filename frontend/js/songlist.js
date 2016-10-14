eb153a670a19e18698df667bd3b4a8f212237a80
Mathias Quintero <mathias.quintero@quartett-mobile.de>
2016-10-14 15:55:07 +0200

    Expanding UI with auto play feature

--- frontend/js/songlist.js
+++ frontend/js/songlist.js
@@ -1,10 +1,22 @@
 var Songlist = function(songs) {
   this.songs = songs;
+  this.songs[0].play();
 };
 
 Songlist.prototype.toHTML = function () {
-  var songsHtml = this.songs.reduce(function(acc, song) {
-    return acc + song.toHTML();
+  var songsHtml = this.songs.reduce(function(acc, song, index) {
+    return acc + song.toHTML(index);
   }, '');
   return '<ul class="collection">' + songsHtml + '</ul>';
+};
+
+Songlist.prototype.play = function (index) {
+  if (this.songs[index].playing) {
+    this.songs[index].pause();
+  } else {
+    for (var i in this.songs) {
+      this.songs[i].pause();
+    }
+    this.songs[index].play();
+  }
 };
