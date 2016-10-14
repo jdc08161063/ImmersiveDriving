f64818667f2415138ae480fa70abba376a51f18b
Georgi Dikov <gvdikov@gmail.com>
2016-10-14 23:33:47 +0200

    imageprep modified

--- backend/classifier/ImagePrep.py
+++ backend/classifier/ImagePrep.py
@@ -1,24 +1,38 @@
 import os
 from PIL import Image
+from scipy import misc
+import numpy as np
 
 class ImagePrep():
 
-    def __init__(self, path_to_images=None):
+    def __init__(self, path_to_images=None, x=224, y=224):
+        self.new_x = x
+        self.new_y = y
         self.images_folder = path_to_images
+        self.images = self._load_resized_images(path_to_images)
+        self.center_all()
 
+    def _load_resized_images(self, path):
 
-    def _resize_to(self, filename=None, x=224, y=224):
-        im = Image.open("./"+self.images_folder+"/"+filename)
-        im = im.resize((x, y), Image.ANTIALIAS)
-        outname = filename[:-8]
-        im.save("./"+self.images_folder+"/resized/"+"{0}.jpg".format(outname))
-
-
-    def resize_all(self, x, y):
-        for filename in os.listdir(self.images_folder):
+        #resize first
+        for i, filename in enumerate(os.listdir(self.images_folder)):
             if filename.endswith(".jpg"):
-                self._resize_to(filename, x=x, y=y)
+                im = Image.open("./" + self.images_folder + "/" + filename)
+                im = im.resize((self.new_x, self.new_y), Image.ANTIALIAS)
+                outname = filename[:-8]
+                im.save("./" + self.images_folder + "/resized/" + "{0}.jpg".format(outname))
 
-if __name__ == "__main__":
-    ip = ImagePrep("./demo_dataset")
-    ip.resize_all(x=224, y=224)
+        # now load as np arrays
+        images = list()
+        for i, filename in enumerate(os.listdir(self.images_folder + "/resized/")):
+            if filename.endswith(".jpg"):
+                images.append(misc.imread(path+"/resized/"+filename))
+        images = np.asarray(images)
+
+        return images
+
+    def center_all(self):
+        # load mean congif data
+        pass
+
+
