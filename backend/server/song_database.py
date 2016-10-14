898b127ffeca86c2374e3483e6dfc0af0f149569
Atanas Mirchev <taimir93@gmail.com>
2016-10-14 23:42:13 +0200

    Add basic songs database

--- backend/server/song_database.py
+++ backend/server/song_database.py
@@ -5,3 +5,24 @@ class Song(object):
         self.album = album
         self.cover = cover
         self.url = url
+
+
+songs_map = {
+    "city_street_urban": [Song("Bomfunk MC", "Freestyler", "-", "-",
+                               "http://www.youtube.com/watch?v=ymNFyxvldaM"),
+                          Song("Eminem", "Lose yourself", "Lose yourself", "-",
+                               "http://www.youtube.com/watch?v=eRhEcKHU5LY"),
+                          Song("Linkin Park", "Numb", "Meteora", "-",
+                               "http://www.youtube.com/watch?v=kXYiU_JCYtU"),
+                          ],
+    "desert_field_void": [],
+    "forest_mountain_nature": [],
+    "night_tunnel_dark": [],
+    "sea_coast_water": [Song("Movie theme", "Pirates of the Carribean", "-", "-",
+                             "http://www.youtube.com/watch?v=27mB8verLK8"),
+                        Song("Movie theme", "Pirates of the Carribean", "-", "-",
+                             "http://www.youtube.com/watch?v=27mB8verLK8"),
+                        ],
+    "surprise_unknown": [],
+    "road_highway_boredom": [],
+}
