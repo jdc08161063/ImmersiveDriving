2c2c5b052da07d79253ac3001dd593f8ecd29e51
Ute Schiehlen <ute.schiehlen@tum.de>
2016-10-19 14:15:47 +0200

    added mp3 version of songs and adapted the song_database.py file

--- backend/server/song_database.py
+++ backend/server/song_database.py
@@ -17,15 +17,15 @@ class Song(object):
 songs_map = {
     "city_street_urban": [
         Song("Imagine Dragons", "Amsterdam", "-", "https://i.ytimg.com/vi/TKtPXO5iEnA/maxresdefault.jpg",
-             "../backend/server/songs/cityStreetUrban/amsterdam.m4a"),
+             "../backend/server/songs/cityStreetUrban/amsterdam.mp3"),
         Song("Abba", "Summer Night City", "-", "http://img.youtube.com/vi/_d5dPYHi17k/0.jpg",
              "../backend/server/songs/cityStreetUrban/summer_night_city.mp3"),
     ],
     "field_plains": [
         Song("Lukas Graham", "7 Years", "-", "http://img.youtube.com/vi/LHCob76kigA/0.jpg",
-             "../backend/server/songs/desertFieldVoid/7years.m4a"),
+             "../backend/server/songs/desertFieldVoid/7years.mp3"),
         Song("Olly Murs", "Right Place Right Time", "-", "http://img.youtube.com/vi/8lxaNO5V06k/0.jpg",
-             "../backend/server/songs/desertFieldVoid/right_place_right_time.m4a")],
+             "../backend/server/songs/desertFieldVoid/right_place_right_time.mp3")],
     "forest_mountain_nature": [
         Song("David Garrett", "Human Nature", "-", "http://img.youtube.com/vi/4RTP1JWn6ZM/0.jpg",
              "../backend/server/songs/forestMountainNature/human_nature.mp3")],
@@ -33,11 +33,11 @@ songs_map = {
         Song("Adele", "Love in the Dark", "-", "http://img.youtube.com/vi/97hZ4bxTqZc/0.jpg",
              "../backend/server/songs/nightTunnelDark/love_in_the_dark.mp3"),
         Song("Adele", "Skyfall", "-", "http://img.youtube.com/vi/DeumyOzKqgI/0.jpg",
-             "../backend/server/songs/nightTunnelDark/skyfall.m4a"),
+             "../backend/server/songs/nightTunnelDark/skyfall.mp3"),
         Song("Imagine Dragons", "Every Night", "-", "http://img.youtube.com/vi/kuijhOvKyYg/0.jpg",
-             "../backend/server/songs/nightTunnelDark/every_night.m4a"),
+             "../backend/server/songs/nightTunnelDark/every_night.mp3"),
         Song("Coldplay", "A Sky Full of Stars", "-", "http://img.youtube.com/vi/97hZ4bxTqZc/0.jpg",
-             "../backend/server/songs/nightTunnelDark/a_sky_full_of_stars.m4a")
+             "../backend/server/songs/nightTunnelDark/a_sky_full_of_stars.mp3")
     ],
     "sea_coast_water": [
         Song("Coldplay", "Every Teardrop is a Waterfall", "-", "http://img.youtube.com/vi/fyMhvkC3A84/0.jpg",
@@ -47,12 +47,12 @@ songs_map = {
     ],
     "surprise_unknown": [
         Song("Wiz Khalifa", "See You Again", "-", "http://img.youtube.com/vi/RgKAFK5djSk/0.jpg",
-             "../backend/server/songs/surprise/see_you_again.m4a"),
+             "../backend/server/songs/surprise/see_you_again.mp3"),
         Song("-", "Ans Ende Der Welt", "-", "",
              "../backend/server/songs/surprise/ans_ende_der_welt.mp3")],
     "road_highway_boredom": [
         Song("Hermes House Band", "Country Roads", "-", "http://img.youtube.com/vi/ZVz_IJoyO6Y/0.jpg",
              "../backend/server/songs/roadHighway/country_roads.mp3"),
         Song("Lena", "Wild And Free", "-", "",
-             "../backend/server/songs/roadHighway/wild_and_free.m4a")]
+             "../backend/server/songs/roadHighway/wild_and_free.mp3")]
 }
